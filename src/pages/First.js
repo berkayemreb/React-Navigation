50ee7ec85d20048433299b15b7612f115096cdb6
bbogum19 <berkaye2001@hotmail.com>
2023-01-09 14:16:57 +0300

    The styling of the 1st page has been done

--- src/pages/First.js
+++ src/pages/First.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 const First = () => {
-    return(
-        <SafeAreaView>
-            <Text> Hello First Page!</Text>
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.text}> Hello First Page!</Text>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 26,
+    }
+})
 
 export default First;
