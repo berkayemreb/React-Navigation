9eab3cc6128d1e08f6d05f4c3e6df1949627a0ef
bbogum19 <berkaye2001@hotmail.com>
2023-01-09 14:36:56 +0300

    The styling of the 2nd page is complete

--- src/pages/Second.js
+++ src/pages/Second.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 const Second = () => {
-    return(
-        <SafeAreaView>
-            <Text> Hello Second Page!</Text>
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.text}> Hello Second Page!</Text>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontStyle: 'italic',
+        fontSize: 20,
+    }
+})
 
 export default Second;
